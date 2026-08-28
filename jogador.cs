a3336cc22a33e3bc523990c1d5673e3f5be2cdce
livia760 <livia.b.ferreira@edu.senai.br>
2026-08-28 11:21:31 -0300

    .

---
+++ jogador.cs
@@ -0,0 +1,18 @@
+﻿namespace abstracao
+{
+    public class Jogador
+    {
+        public string Nome { get; set; }
+        public string Posicao { get; set; }
+        public int NumeroCamisa { get; set; }
+}
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Jogador: {nome}");
+            Console.WriteLine($"Posição: {posicao}");
+            Console.WriteLine($"Número da Camisa: {numeroCamisa}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
---
+++ time.cs
@@ -0,0 +1,33 @@
+﻿namespace abstracao
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Time
+    {
+        public string NomeDoTime { get; set; }
+        public List<Jogador> ListaDeJogadores { get; set; }
+
+        public Time()
+        {
+            ListaDeJogadores = new List<Jogador>();
+        }
+
+        public void AdicionarJogador(Jogador jogador)
+        {
+            ListaDeJogadores.Add(jogador);
+        }
+
+        public void ListarJogadores()
+        {
+            Console.WriteLine("Jogadores do time:");
+
+            foreach (Jogador jogador in ListaDeJogadores)
+            {
+                Console.WriteLine("Nome: " + jogador.Nome);
+                Console.WriteLine("Posição: " + jogador.Posicao);
+                Console.WriteLine();
+            }
+        }
+    }
+}
